882fe59ce00a791120d73f0613596a4550ae62a0
takabayap <takabayap@gmail.com>
2018-11-06 11:30:32 +0900

    add Scene

---
+++ main.js
@@ -0,0 +1,40 @@
+"use strict";
+phina.globalize();
+const Assets={
+    Debug:false,
+    Fps:60,
+    ScreenWidth:1500,
+    ScreenHeight:960,
+    MyScenes:[
+        {
+            label:"PhinaSplash",
+            className:"SplashScene"
+        },
+        {
+            label:"Game",
+            className:"GameScene",
+        }
+    ],
+    DebugSettings:{
+        log_path:"",
+        target_stage:1,
+    }
+};
+phina.define("GameScene",{
+    superClass:"DisplayScene",
+    init:function(options){
+        this.superInit(options);
+    }
+});
+phina.main(function(){
+    let app=GameApp({
+        startLabel:"PhinaSplash",
+        assets:Assets,
+        width:Assets.ScreenWidth,
+        height:Assets.ScreenHeight,
+        scenes:Assets.MyScenes,
+    });
+    app.fps=Assets.Fps;
+    if(Assets.Debug)app.enableStats();
+    app.run();
+});
