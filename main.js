866ecb13c70cb1572a15d143abae273d8d230896
takabayap <takabayap@gmail.com>
2018-11-11 23:03:08 +0900

    add background class

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 "use strict";
 phina.globalize();
-const Assets={
+const Conf={
     Debug:false,
     Fps:60,
     ScreenWidth:1500,
     ScreenHeight:960,
+    PlayAreaWidth:1000,
+    PlayAreaHeight:1000,
+    ScrollSpeed:15,
     MyScenes:[
         {
             label:"PhinaSplash",
@@ -18,23 +21,51 @@ const Assets={
     DebugSettings:{
         log_path:"",
         target_stage:1,
-    }
+    },
+    Stages:[
+
+
+    ]
+};
+const Assets={
+    image:{
+        Bg1:"img/back2.png"
+    },
 };
 phina.define("GameScene",{
     superClass:"DisplayScene",
     init:function(options){
         this.superInit(options);
+        this.options=options;
+        if(Conf.Debug)this.options.stage=Conf.DebugSettings.target_stage;
+        this.stageMap=Conf.Stages[this.options.stage];
+        console.log(new Background());
+    }
+});
+phina.define("Background",{
+    superClass:"Sprite",
+    init:function(options){
+        this.superInit("Bg1");//options.image);
+        //this.width=options.width;
+        //this.height=options.height;
+        this.setOrigin(0,1);
+    },
+    _static:{
+        defaults:{
+            width:Conf.PlayAreaWidth,
+            height:this._height*(Conf.PlayAreaWidth/this._width),
+        }
     }
 });
 phina.main(function(){
     let app=GameApp({
         startLabel:"PhinaSplash",
-        //assets:Assets,
-        width:Assets.ScreenWidth,
-        height:Assets.ScreenHeight,
-        scenes:Assets.MyScenes,
+        assets:Assets,
+        width:Conf.ScreenWidth,
+        height:Conf.ScreenHeight,
+        scenes:Conf.MyScenes,
     });
-    app.fps=Assets.Fps;
-    if(Assets.Debug)app.enableStats();
+    app.fps=Conf.Fps;
+    if(Conf.Debug)app.enableStats();
     app.run();
 });
